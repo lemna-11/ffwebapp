aad099f9c78a4b4c46b628d90dcc02a35857d676
Mano <lemmensmano@gmail.com>
2023-06-08 20:50:40 +0200

    and i oop

---
+++ src/main/java/com/example/ffwebapp/views/MainPage.java
@@ -0,0 +1,17 @@
+package com.example.ffwebapp.views;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainPage extends AppLayout {
+    public MainPage(){
+        System.out.println("krill yourself");
+        addToNavbar(new H1("why"));
+        addToDrawer(new Button("krill"));
+        setContent(new Text("hello world"));
+    }
+}
