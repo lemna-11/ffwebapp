fda31b95e683ac3d74bd3569ae2b8a6b49e0531c
CookiesCream1 <luky.pospa04@gmail.com>
2023-08-13 15:08:53 +0200

    need to fix the stuff..

--- src/main/java/com/example/ffwebapp/views/CheckoutPage.java
+++ src/main/java/com/example/ffwebapp/views/CheckoutPage.java
@@ -1,31 +1,52 @@
 package com.example.ffwebapp.views;
 
+import com.example.ffwebapp.middleware.callers.CategoryCaller;
+import com.example.ffwebapp.middleware.callers.OrderCaller;
+import com.example.ffwebapp.middleware.entities.Order;
+import com.example.ffwebapp.middleware.entities.Product;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
+import org.jsoup.select.CombiningEvaluator;
+
+import javax.swing.border.Border;
 
 @Route("checkout")
-public class CheckoutPage extends AppLayout {
-    public CheckoutPage(){
-        HorizontalLayout layout = new HorizontalLayout();
-        layout.setPadding(true);
-        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
-        layout.add(new Button("PLACEHOLDER"));
-        layout.add(new Button("PLACEHOLDER"));
-        layout.add(new Button("PLACEHOLDER"));
-        layout.add(new Button("PLACEHOLDER"));
-        layout.add(new Button("PLACEHOLDER"));
-        layout.add(new Button("PLACEHOLDER"));
-        layout.add(new Button("PLACEHOLDER"));
-        layout.add(new Button("PLACEHOLDER"));
-        layout.add(new Button("PLACEHOLDER"));
-        layout.add(new Button("PLACEHOLDER"));
-        layout.add(new Button("PLACEHOLDER"));
 
-        layout.add(new Button("pay."));
-        addToDrawer(layout);
+public class CheckoutPage extends AppLayout {
+
+    public CheckoutPage(OrderCaller orderCaller, CategoryCaller categoryCaller) {
+        DrawerToggle toggle = new DrawerToggle();
+
+        H1 title = new H1("your order");
+        title.getStyle().set("font-size", "var(--lumo-font-size-l)")
+                .set("margin", "0");
+        Tabs cats = categoryCaller.readAll(id);
+
+        addToDrawer(cats);
+        addToNavbar(toggle, title);
+
+        setPrimarySection(Section.DRAWER);
+
+        Grid<Order> grid = new Grid<>(Order.class, false);
+        grid.addColumn(Order::getName).setHeader("Product Name");
+        grid.addColumn(Order::getPriceInCents).setHeader("Product Price");
+        grid.addColumn(Order::getCategory).setHeader("Product Category");
+
+        Button cancel = new Button("Cancel your order");
+        cancel.setAutofocus(true);
+        cancel.addClickListener(event -> {
+            UI.getCurrent().navigate("");
+            orderCaller.delete(Order.getId());
+        });
+
 
     }
 }
--- src/main/java/com/example/ffwebapp/views/MainPage.java
+++ src/main/java/com/example/ffwebapp/views/MainPage.java
@@ -3,35 +3,77 @@ package com.example.ffwebapp.views;
 import com.example.ffwebapp.middleware.callers.CategoryCaller;
 import com.example.ffwebapp.middleware.callers.OrderCaller;
 import com.example.ffwebapp.middleware.callers.ProductCaller;
-import com.vaadin.flow.component.Text;
+import com.example.ffwebapp.middleware.entities.Order;
+import com.example.ffwebapp.middleware.entities.OrderStatus;
+import com.example.ffwebapp.middleware.entities.Product;
+import com.example.ffwebapp.middleware.entities.ProductCategory;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
 
+
+import java.util.List;
+import java.util.Optional;
+
+
 @Route("")
+
 public class MainPage extends AppLayout {
-    public MainPage(CategoryCaller categoryCaller, OrderCaller orderCaller, ProductCaller productCaller){
-        addToNavbar(new H1("why"));
-        Button toCheckout = new Button("Check out");
-        toCheckout.addClickListener(e -> getUI().orElseThrow().navigate(CheckoutPage.class));
-        addToNavbar(toCheckout);
-        Button toOrder = new Button("Order");
-        toCheckout.addClickListener(e -> getUI().orElseThrow().navigate(OrderPage.class));
-        addToNavbar(toOrder);
-        addToDrawer(new Button("krill"));
-        Button order = new Button("order");
-        order.addClickListener(event -> {
-            System.out.println("sys");
-            categoryCaller.create("amogus");
-        });
-        Button checkout = new Button("checkout");
-        order.addClickListener(event -> {
-            System.out.println("sys");
-            categoryCaller.create("amogus");
-        });
-        addToDrawer(order);
+    private Order order;
+
+    public MainPage(CategoryCaller categoryCaller, OrderCaller ordercaller, ProductCaller productCaller) {
+        DrawerToggle toggle = new DrawerToggle();
+
+
+        H1 title = new H1("Product");
+        title.getStyle().set("font-size", "var(--lumo-font-size-l)")
+                .set("margin", "0");
+
+
+        Tabs cats = categoryCaller.readAll(id);
+
+        Button checkout = new Button("Checkout");
+            checkout.addClickListener(event -> {
+                UI.getCurrent().navigate("checkout");
+            });
+
+        checkout.setAutofocus(true);
+
+        addToDrawer(cats);
+        addToNavbar(toggle, title);
         addToDrawer(checkout);
-        setContent(new Text("hello world"));
+        setPrimarySection(Section.DRAWER);
+
+
+        Grid<Product> grid = new Grid<>(Product.class, false);
+        grid.addColumn(Product::getName).setHeader("Product Name");
+        grid.addColumn(Product::getPriceInCents).setHeader("Product Price");
+        grid.addColumn(Product::getCategory).setHeader("Product Category");
+
+        grid.addSelectionListener((Product) ->{
+
+            Optional<Product>select = Product.getFirstSelectedItem();
+            if (select.isPresent()){
+                if(order==null){
+                    Long id = ordercaller.create(List.of(select.get()), OrderStatus.IN_PROCESS);
+                    this.order = new Order(id, List, OrderStatus.IN_PROCESS);
+                }
+
+            }
+         }
+        );
+
+        List<Product> products = ;
+        grid.setItems(Product);
+        grid.setAllRowsVisible(true);
+        setContent(grid);
+
     }
+
+
 }
